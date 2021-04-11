33106e4420cc76073e7769cb711035ec9aee8efa
la8h <50991583+la8h@users.noreply.github.com>
2021-04-11 12:23:10 -0400

    Create project for assignment 4

---
+++ LaythAssign44/src/main/java/layth/mahfoodh/s991523983/GalleryFragment.java
@@ -0,0 +1,275 @@
+package layth.mahfoodh.s991523983;
+
+import android.Manifest;
+import android.app.ProgressDialog;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.location.Location;
+import android.os.AsyncTask;
+import android.os.Bundle;
+
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.Fragment;
+
+import android.text.format.DateFormat;
+import android.view.LayoutInflater;
+import android.view.View;
+/*
+Layth Mahfoodh s991523983 PROG38448
+ */
+
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+//import com.google.android.gms.location.LocationRequest;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link GalleryFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public  class GalleryFragment extends Fragment{ //} implements OnMapReadyCallback{
+
+    URL ImageUrl = null;
+    InputStream is = null;
+    Bitmap bmImg = null;
+    ImageView imageView= null;
+    ProgressDialog p;
+
+
+    /*
+    //add last
+    public static final String TAG = "WhereAmIActivity";
+    private static final String ERROR_MSG = "Google Play services are unavailable.";
+    private static final int LOCATION_PERMISSION_REQUEST = 1;
+    private static final int REQUEST_CHECK_SETTINGS = 2;
+
+    private TextView mTextView;
+    private LocationRequest mLocationRequest;
+    private GoogleMap mMap;
+
+    private List<Marker> mMarkers = new ArrayList<>();
+    private Polyline mPolyline;
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(17));
+
+        if (ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED ||
+                ActivityCompat.checkSelfPermission(this,
+                        Manifest.permission.ACCESS_COARSE_LOCATION)
+                        == PackageManager.PERMISSION_GRANTED) {
+            mMap.setMyLocationEnabled(true);
+        }
+        PolylineOptions polylineOptions = new PolylineOptions()
+                .color(Color.CYAN)
+                .geodesic(true);
+        mPolyline = mMap.addPolyline(polylineOptions);
+    }
+
+    LocationCallback mLocationCallback = new LocationCallback() {
+        @Override
+        public void onLocationResult(LocationResult locationResult) {
+            Location location = locationResult.getLastLocation();
+            if (location != null) {
+                updateTextView(location);
+            }
+
+            if (location != null) {
+                updateTextView(location);
+                if (mMap != null) {
+                    LatLng latLng = new LatLng(location.getLatitude(),
+                            location.getLongitude());
+                    mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+
+                    Calendar c = Calendar.getInstance();
+                    String dateTime = DateFormat.format("MM/dd/yyyy HH:mm:ss",
+                            c.getTime()).toString();
+
+                    int markerNumber = mMarkers.size()+1;
+                    mMarkers.add(mMap.addMarker(new MarkerOptions()
+                            .position(latLng)
+                            .title(dateTime)
+                            .snippet("Marker #" + markerNumber +
+                                    " @ " + dateTime)));
+
+                    List<LatLng> points = mPolyline.getPoints();
+                    points.add(latLng);
+                    mPolyline.setPoints(points);
+                }
+            }
+        }
+    };
+
+*/
+
+
+
+
+
+
+
+
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public GalleryFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment GalleryFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static GalleryFragment newInstance(String param1, String param2) {
+        GalleryFragment fragment = new GalleryFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_gallery, container, false);
+
+        final Spinner spin = (Spinner) view.findViewById(R.id.provinces_spinner);
+        //create a button object
+        final Button submit = (Button)view.findViewById(R.id.submit);
+
+        imageView=view.findViewById(R.id.imageView2);
+
+        //handle the  click event
+        submit.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                AsyncTaskExample asyncTask=new AsyncTaskExample();
+                asyncTask.execute("https://www.tutorialspoint.com/images/tp-logo-diamond.png");
+
+
+                //get the spinner view as text view
+                TextView text_sel = (TextView)spin.getSelectedView();
+                //get the text from the spinner view
+           //     Toast.GalleryFragment.this, "\n Province = "+text_sel.getText(), Toast.LENGTH_SHORT).show();
+           //     Toast.makeText(GalleryFragment.this, "\n Province = "+text_sel.getText(), Toast.LENGTH_SHORT).show();
+             //   Toast.makeText(MainActivity.this, "\n Province = "+text_sel.getText(), Toast.LENGTH_SHORT).show();
+                Toast.makeText(getActivity(), "\n Province = "+text_sel.getText(), Toast.LENGTH_SHORT).show();
+                //MainActivity.this   getActivity()
+            }
+
+        });
+
+        return view;
+
+    }
+
+
+
+
+    private class AsyncTaskExample extends AsyncTask<String, String, Bitmap> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+           // p=new ProgressDialog(MainActivity.this);
+
+            p=new ProgressDialog(getActivity());
+
+            p.setMessage("Please wait...It is downloading");
+            p.setIndeterminate(false);
+            p.setCancelable(false);
+            p.show();
+        }
+
+        @Override
+        protected Bitmap doInBackground(String... strings) {
+            try {
+
+                ImageUrl = new URL(strings[0]);
+                HttpURLConnection conn = (HttpURLConnection) ImageUrl
+                        .openConnection();
+                conn.setDoInput(true);
+                conn.connect();
+                is = conn.getInputStream();
+
+                BitmapFactory.Options options = new BitmapFactory.Options();
+                options.inPreferredConfig = Bitmap.Config.RGB_565;
+                bmImg = BitmapFactory.decodeStream(is, null, options);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return bmImg;
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap bitmap) {
+            super.onPostExecute(bitmap);
+            if(imageView!=null) {
+                p.hide();
+                imageView.setImageBitmap(bitmap);
+            }else {
+                p.show();
+            }
+        }
+    }
+
+
+
+
+
+
+}
---
+++ LaythAssign44/src/main/java/layth/mahfoodh/s991523983/LaythActivity.java
@@ -0,0 +1,280 @@
+package layth.mahfoodh.s991523983;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+/*
+Layth Mahfoodh s991523983 PROG38448
+ */
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.navigation.NavigationView;
+
+import layth.mahfoodh.s991523983.ui.login.LoginFragment;
+
+public class LaythActivity extends AppCompatActivity {
+
+    private DrawerLayout mDrawerLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //set default fragment
+        loadFragment(new CameraFragment());
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar); // to have the drawer sliding when click on ic_menu
+
+        // if comment out these 3 lines, your drawer is not pulled from action bar!
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.setDisplayHomeAsUpEnabled(true);
+
+        // check what happens if you comment out line below (change to Up Key)
+        actionbar.setHomeAsUpIndicator(R.drawable.ic_menu);
+
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        // set item as selected to persist highlight
+                        menuItem.setChecked(true);
+                        // close drawer when item is tapped
+                        mDrawerLayout.closeDrawers();
+
+                        // Add code here to update the UI based on the item selected
+                        // For example, swap UI fragments here
+                        //display in short period of time
+                        Toast.makeText(getApplicationContext(), menuItem.getTitle(),
+                                Toast.LENGTH_LONG).show();
+
+                        // Launch the corresponding fragment
+                        Fragment fragment = null;
+                        Class fragmentClass = null;
+                        switch (menuItem.getItemId()) {
+                            case R.id.nav_camera: // display ad
+                                fragmentClass = CameraFragment.class;
+                                break;
+                            case R.id.nav_gallery: // canada
+                                fragmentClass = GalleryFragment.class;
+                                break;
+                            case R.id.nav_manage: // Map
+                                fragmentClass = MessageFragment.class;
+                                break;
+                            default:
+                                fragmentClass = LoginFragment.class;
+                        }
+
+                        try {
+                            fragment = (Fragment) fragmentClass.newInstance();
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+
+                        // Insert the fragment by replacing any existing fragment
+                       loadFragment(fragment);
+
+
+                        return true;
+                    }
+                });
+
+
+        mDrawerLayout.addDrawerListener(
+                new DrawerLayout.DrawerListener() {
+                    @Override
+                    public void onDrawerSlide(View drawerView, float slideOffset) {
+                        // Respond when the drawer's position changes
+                    }
+
+                    @Override
+                    public void onDrawerOpened(View drawerView) {
+                        // Respond when the drawer is opened
+                    }
+
+                    @Override
+                    public void onDrawerClosed(View drawerView) {
+                        // Respond when the drawer is closed
+                    }
+
+                    @Override
+                    public void onDrawerStateChanged(int newState) {
+                        // Respond when the drawer motion state changes
+                    }
+                }
+        );
+
+
+
+    }
+
+    // When main drawer is opened
+    // when press on 3 lines, what to do
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        mDrawerLayout.openDrawer(GravityCompat.START);
+
+        switch (item.getItemId()) {
+            case android.R.id.home: // START to slide from left
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+
+
+        }
+        return super.onOptionsItemSelected(item);
+
+
+    }
+
+    public void open(){
+
+        Toast.makeText(this, "You selected Speed up!!", Toast.LENGTH_LONG).show();
+
+    }
+
+    public void loadFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.flContent, fragment);
+        transaction.commit();
+    }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the tools bar if it is present.
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+/*
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle tools bar item clicks here. The tool bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        switch (item.getItemId())
+        {
+            case R.id.start:
+                //displayTextView.setText("You selected start!");
+                Toast.makeText(this, "You selected start!", Toast.LENGTH_LONG).show();
+                break;
+            case R.id.accel:
+                Toast.makeText(this, "You selected Speed up!!", Toast.LENGTH_LONG).show();
+                break;
+            case R.id.stop: Toast.makeText(this, "You selected stop!", Toast.LENGTH_LONG).show();
+                break;
+            case R.id.decel:
+                Toast.makeText(this, "You selected Slow Down!", Toast.LENGTH_LONG).show();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+*/
+
+
+    public void callIntent(View view) {
+        Intent intent = null;
+        switch (view.getId()) {
+            case R.id.start:
+                intent = new Intent(Intent.ACTION_VIEW,
+                        Uri.parse("https://www.cbc.ca"));
+                startActivity(intent);
+                break;
+            case R.id.accel:
+                intent = new Intent(Intent.ACTION_DIAL,
+                        Uri.parse("tel:(416)289-5000"));
+                startActivity(intent);
+                break;
+            case R.id.decel:
+                intent = new Intent(Intent.ACTION_VIEW,
+                        Uri.parse("https://www.cp24.ca"));
+
+                startActivity(intent);
+            default:
+                break;
+        }
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        Log.d("CDA", "onBackPressed Called");
+        // create alert dialog
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(LaythActivity.this);
+        builder.setTitle("Do you want to exit?");
+
+
+        builder.setMessage("Click yes to exit!")
+                .setCancelable(false)
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //if user pressed "yes", then he is allowed to exit from application
+                        finish();
+                    }
+
+                    /*
+                    public void onClick(DialogInterface dialog, int id) {
+
+                        //display in short period of time
+                        Toast.makeText(getApplicationContext(), "Yes is clicked",
+                                Toast.LENGTH_LONG).show();
+
+                    } */
+                })
+
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // if this button is clicked, just close
+                        // the dialog box and do nothing
+                        dialog.cancel();
+
+                        //display in short period of time
+                        Toast.makeText(getApplicationContext(), "No is clicked",
+                                Toast.LENGTH_LONG).show();
+                    }
+                });
+
+
+// create alert dialog
+        AlertDialog alertDialog = builder.create();
+
+
+
+
+        alertDialog.show();
+
+      //  builder.show();
+    }
+
+
+
+
+
+}
